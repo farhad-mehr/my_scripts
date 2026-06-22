1735431532a51deb37da2a2a3eddada62805ac32
farhad <farhad.mehr1997@gmail.com>
2026-06-22 17:57:37 +0330

    add -y to docker installing proccess (:

--- rdp_lxqt/script.sh
+++ rdp_lxqt/script.sh
@@ -145,7 +145,7 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 systemctl enable docker
 systemctl restart docker
