83ee4bba758ea9cf70ddbd66b75f43106a2d2620
farhad <farhad.mehr1997@gmail.com>
2026-06-22 17:11:28 +0330

    some changes for installing docker

--- rdp_lxqt/script.sh
+++ rdp_lxqt/script.sh
@@ -119,3 +119,33 @@ if [[ $? -ne 0 ]]; then
   echo "Failed to restart xrdp."
   exit 1
 fi
+
+
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+if [[ $? -ne 0 ]]; then
+  echo "apt uninstall all older docker related packages failed."
+  exit 1
+fi
+
+
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker
+systemctl restart docker
